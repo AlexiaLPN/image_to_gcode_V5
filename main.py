b67b00cce74f720d62813382f96ae81dcb36995b
AlexiaLPN <biochimie@lapatisserienumerique.com>
2025-08-20 08:51:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -325,7 +325,7 @@ st.markdown(
         .logo-container {
             position: fixed;
             top: 55px;
-            left: 50px;
+            left: 70px;
             z-index: 100;
         }
     </style>
